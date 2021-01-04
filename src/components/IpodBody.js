2d9821687695e7ab34bc1d782aa940fbc703013f
nidhiverma1997 <nidhiverma1997@gmail.com>
2021-01-05 00:21:39 +0530

    Add iPod Body component

---
+++ src/components/IpodBody.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class IpodBody extends Component {
+  render() {
+    return <div className="ipodBody flex"></div>;
+  }
+}
+
+export default IpodBody;

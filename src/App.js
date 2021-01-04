1736929d306caad5add8fa48b103b9a9c63a5ab3
nidhiverma1997 <nidhiverma1997@gmail.com>
2021-01-05 00:22:09 +0530

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./index.scss";
+import "./components/IpodBody";
+import IpodBody from "./components/IpodBody";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App flex">
+      <IpodBody />
+    </div>
+  );
 }
 
 export default App;
